d1800712b06f4b60ec49af0c5bffdad9e561c85d
Tajbir islam <studenttajbirislam@gmail.com>
2024-04-29 06:53:29 +0600

    fix some design

--- src/pages/ArtCraft/Category.jsx
+++ src/pages/ArtCraft/Category.jsx
@@ -5,23 +5,24 @@ const Category = () => {
     const category = useLoaderData();
     const {id} = useParams()
     
+    console.log(category)
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center m-20 mt-32">
         <Helmet>
             <title>{id}</title>
         </Helmet>
             {category.map((item) =>  {
-                const { image, itemName, subcategoryName, shortDescription, price, rating, processingTime } = item;
+                const { image, item_name, subcategory_name, short_description, price, rating, processing_time } = item;
                 return (
-                    <div key={item?._id} className="max-w-sm rounded overflow-hidden flex flex-col flex-grow shadow-lg">
-                        <img className="w-[300px] h-[300px]" src={image} alt={itemName} />
+                    <div key={item?._id} className="w-full rounded overflow-hidden flex flex-col flex-grow shadow-lg">
+                        <img className="w-full h-[300px]" src={image} alt={item_name} />
                         <div className="px-6 flex-grow py-4">
-                            <div className="font-bold text-xl mb-2">{itemName}</div>
-                            <p className=" text-base mb-2">{subcategoryName}</p>
-                            <p className=" text-base mb-2">{shortDescription}</p>
+                            <div className="font-bold text-xl mb-2">{item_name}</div>
+                            <p className=" text-base mb-2 font-semibold">Subcategory : {subcategory_name}</p>
+                            <p className=" text-base mb-2">{short_description}</p>
                             <p className=" font-bold text-xl mb-2">${price}</p>
                             <p className=" text-base mb-2">Rating: {rating} ⭐</p>
-                            <p className=" text-base mb-2">Processing Time: {processingTime}</p>
+                            <p className=" text-base mb-2">Processing Time: {processing_time}</p>
                         </div>
                         <div className="px-6 py-4 mb-5">
                             <Link to={`/view_details/${item?._id}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
